f96767046e10a7396b09824bad558973dc7bc79d
Mel <57514727+Mel1207@users.noreply.github.com>
2024-11-10 21:22:07 +0800

    added postTask from input value

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,16 @@
 import { createStore } from "vuex"
+import Task from "../types/Task"
 
 export default createStore({
   state: {
+    tasks: <Task[]>[],
+
     showModal: false,
+    newTask: {
+      title: '',
+      priorityLevel: 'low',
+      status: 'todo'
+    }
   },
   mutations: {
     openModal(state: { showModal: boolean }) {
--- src/views/PageTaskBoard.vue
+++ src/views/PageTaskBoard.vue
@@ -8,8 +8,7 @@
           <h1 class="text-xl font-semibold mb-[3px]">Welcome back John 👋</h1>
           <p class="text-sm text-[#22222250]">Lets track your task today</p>
         </div>
-        <!-- <ButtonSet btn-title="New task" has-icon class-list="h-[40px] px-[15px] bg-primary text-white flex gap-[10px] items-center rounded-lg hover:opacity-80 transition" @click="$store.commit('openModal')"/> -->
-        <ButtonSet btn-title="New task" has-icon class-list="h-[40px] px-[15px] bg-primary text-white flex gap-[10px] items-center rounded-lg hover:opacity-80 transition" @click="handleAdd"/>
+        <ButtonSet btn-title="New task" has-icon class-list="h-[40px] px-[15px] bg-primary text-white flex gap-[10px] items-center rounded-lg hover:opacity-80 transition" @click="$store.commit('openModal')"/>
       </div>
       <div class="grid grid-cols-3 gap-5 mb-5">
         <div class="rounded-lg bg-green-100 px-5 flex items-center h-[50px]">
@@ -46,7 +45,7 @@
           </div>
         </div>
         <div class="border rounded-[15px] p-3 flex flex-col gap-[10px]">
-          <div v-for="item in doneTasks" :key="Number(item.id)" class="border rounded-lg p-[15px] flex justify-between items-start" @click="deleteTask(item.id)">
+          <div v-for="item in doneTasks" :key="String(item.id)" class="border rounded-lg p-[15px] flex justify-between items-start" @click="deleteTask(item.id)">
             <div>
               <p class="text-sm font-semibold mb-[5px]">{{ item.title }}</p>
               <span class="block text-[#FF6581] text-xs py-[5px] px-2 rounded-full bg-[#FF658115] w-max">{{ item.priorityLevel }} priority</span>
@@ -65,40 +64,22 @@
 import ButtonSet from '../components/buttons/ButtonSet.vue'
 import Modal from '../components/modal/Modal.vue'
 // import ModalConfirm from '../components/modal/ModalConfirm.vue'
-import { computed, onMounted, ref } from 'vue'
-import Task from '../types/Task'
+import { computed, onMounted } from 'vue'
+import store from '../store'
 
-const tasks = ref<Task[]>([])
-const newTask = ref({
-  title: 'submit test',
-  status: 'ongoing',
-  priorityLevel: 'low'
-})
 
 const getTasks = async () => {
   await fetch('http://localhost:3000/tasks')
     .then(res => res.json())
-    .then(data => tasks.value = data)
+    .then(data => store.state.tasks = data)
+    console.log(store.state.tasks)
 }
 
-const postTask = async (task: { title: string, status: string, priorityLevel: string}) => {
-  const response = await fetch('http://localhost:3000/tasks', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(task)
-  })
-  const createdTask = await response.json()
-  tasks.value.push(createdTask)
-  // const createdTask = await
-}
+const todoTasks = computed(() => store.state.tasks.filter((item: { status: string; }) => item.status === 'todo'))
 
-const todoTasks = computed(() => tasks.value.filter(item => item.status === 'todo'))
+const onGoingTasks = computed(() => store.state.tasks.filter((item: { status: string; }) => item.status === 'ongoing'))
 
-const onGoingTasks = computed(() => tasks.value.filter(item => item.status === 'ongoing'))
-
-const doneTasks = computed(() => tasks.value.filter(item => item.status === 'done'))
+const doneTasks = computed(() => store.state.tasks.filter((item: { status: string; }) => item.status === 'done'))
 
 const deleteTask = async (id: String) => {
   try {
@@ -109,20 +90,14 @@ const deleteTask = async (id: String) => {
       throw new Error('Failed to delete task')
     }
     // Update the tasks array by removing the deleted task
-    tasks.value = tasks.value.filter(task => task.id !== id)
+    store.state.tasks = store.state.tasks.filter((task: { id: String; }) => task.id !== id)
   } catch (err) {
     console.error(err)
   }
 }
 
-const handleAdd = () => {
-  postTask(newTask.value)
-  console.log('test')
-}
-
 onMounted(() => {
   getTasks()
-  console.log(onGoingTasks)
   // console.log('component is running')
 })
 
